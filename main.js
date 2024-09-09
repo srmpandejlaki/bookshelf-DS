71180935eb93624b7550c7520ef7be8a519f3228
Mutiara <mutiarapandejlaki27@gmail.com>
2024-09-09 08:46:18 +0800

    fix js tmbh data buku sampai tampilkan data di console

--- main.js
+++ main.js
@@ -11,10 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // function untuk tambah buku
   function addBook() {
-    const title = document.getElementById("booFormTitle").value;
+    const title = document.getElementById("bookFormTitle").value;
     const author = document.getElementById("bookFormAuthor").value;
     const year = document.getElementById("bookFormYear").value;
-    const checklist = document.getElementById("bookFormIsComplete").value;
 
     const generateId = generateID();
     const bookObject = generateBookObject(
@@ -22,11 +21,26 @@ document.addEventListener("DOMContentLoaded", function () {
       title,
       author,
       year,
-      checklist
+      false
     );
     shelf.push(bookObject);
+
+    // render data
+    document.dispatchEvent(new Event(RENDER_EVENT));
   }
 
-  // render data
-  document.dispatchEvent(new Event(RENDER_EVENT));
+  // function untuk membuat id unik
+  function generateID() {
+    return +new Date();
+  }
+
+  // function untuk membuat objek buku
+  function generateBookObject(id, judul, penulis, tahun, isCompleted) {
+    return { id, judul, penulis, tahun, isCompleted };
+  }
+
+  // function untuk menampilkan data buku
+  document.addEventListener(RENDER_EVENT, function () {
+    console.log(shelf);
+  });
 });
